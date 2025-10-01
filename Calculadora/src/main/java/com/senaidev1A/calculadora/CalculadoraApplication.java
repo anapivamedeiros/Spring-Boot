455775a620ba27cbf64dce5ac4c07a02dc2c294c
anapivamedeiros <ana.piva1a@gmail.com>
2025-10-01 10:26:27 -0300

    CALCULADORA2

--- Calculadora/src/main/java/com/senaidev1A/calculadora/CalculadoraApplication.java
+++ Calculadora/src/main/java/com/senaidev1A/calculadora/CalculadoraApplication.java
@@ -2,6 +2,7 @@ package com.senaidev1A.calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class CalculadoraApplication {
--- Calculadora/src/main/java/com/senaidev1A/calculadora/CalculadoraControllers.java
+++ Calculadora/src/main/java/com/senaidev1A/calculadora/CalculadoraControllers.java
@@ -1,8 +1,14 @@
 package com.senaidev1A.calculadora;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CalculadoraControllers {
@@ -23,5 +29,34 @@ public class CalculadoraControllers {
 	public double divisao(@RequestParam double a,@RequestParam double b) {
 		return a / b;
 	}
+	
+	@PostMapping("/Calcular")
+	@ResponseBody
+	
+	public Map<String, Object> calcularJson(
+			@RequestParam double num1,
+			@RequestParam double num2,
+			@RequestParam String operacao){
+		
+		double resultado=0;
+		String erro= null;
+		
+		switch(operacao){
+			case "somar": resultado = num1 + num2; break;
+			case "subtrair": resultado = num1 - num2; break;
+			case "multiplicar": resultado = num1 * num2; break;
+			case "dividisao":
+				if(num2==0) erro = "Divisão por zero não é permitido. ";
+				else resultado = num1/num2; break ;
+				default: erro= "Operação inválida";
+				
+		}
+		Map<String, Object> resp = new HashMap<>();
+		resp.put(", resp: ", resultado);
+		resp.put("erro:  ", erro);
+		return resp;
+			
+		}
+	}
 
-}
+
